60bdc0abf24c311feca2dc58fbbce5ee69c70905
MmDZ21 <mmohamadzz294@gmail.comgit config --global user.name MmDZ21git config --global user.email mmohamadzz294@gmail.com>
2025-07-29 19:42:12 +0200

    update

--- app/api/departments/new/route.ts
+++ app/api/departments/new/route.ts
@@ -4,9 +4,9 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { prisma } from "@/lib/prisma";
 export const POST = async (req: NextRequest) => {
   const session = await getServerSession(authOptions);
-  if (!session) {
-    return new NextResponse("شما مجاز به این عملیات نیستید", { status: 403 });
-  }
+  // if (!session) {
+  //   return new NextResponse("شما مجاز به این عملیات نیستید", { status: 403 });
+  // }
   try {
     const formData = await req.formData();
     const name = formData.get("name") as string;
